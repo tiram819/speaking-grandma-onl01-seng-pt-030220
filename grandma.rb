d3b35b14cccdb25f064199f9d087e9b85431f85a
Tabassum Hassan <tiram819@gmail.com>
2020-03-06 05:34:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,12 +18,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.upcase
-    
-  return "NO, NOT SINCE 1938!"
-else
+    return "NO, NOT SINCE 1938!"
+  else
   return answer
-end
-end
 
 
   
