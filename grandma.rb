c6e7f06f6e695b53c40b5ffaa6eb5ece2fe70e1e
Tabassum Hassan <tiram819@gmail.com>
2020-03-06 05:37:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  answer "HUH?! SPEAK UP, SONNY!."
+  answer "HUH?! SPEAK UP, SONNY!"
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.upcase
